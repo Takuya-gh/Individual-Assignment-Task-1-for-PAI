7801f1eec5acce4eaeabc09ed877b164ad9f3ae4
Takuya Otsuki <tsukky333@gmail.com>
2025-12-12 22:36:06 +0000

    test: add tests for Analyzer.group_aggregate

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -52,6 +52,25 @@ class TestAnalyzer(unittest.TestCase):
         self.assertAlmostEqual(result.iloc[0]["value"], 48.0)  # mean of 64.0 and 32.0
         self.assertAlmostEqual(result.iloc[1]["value"], 49.0)  # mean of 65.0 and 33.0
 
+    def test_group_aggregate_groups_by_country(self):
+        """Test that group_aggregate() groups by specified columns."""
+        df = pd.DataFrame({
+            "country_code": ["ABW", "ABW", "AFG", "AFG"],
+            "value": [64.0, 65.0, 32.0, 33.0]
+        })
+
+        result = self.analyzer.group_aggregate(df, group_cols=["country_code"], agg_col="value")
+
+        # Should have 2 rows (one per country)
+        self.assertEqual(len(result), 2)
+
+        # Check aggregated values (mean)
+        abw_row = result[result["country_code"] == "ABW"]
+        afg_row = result[result["country_code"] == "AFG"]
+        
+        self.assertAlmostEqual(abw_row["value"].iloc[0], 64.5)  # mean of 64.0 and 65.0
+        self.assertAlmostEqual(afg_row["value"].iloc[0], 32.5)  # mean of 32.0 and 33.0
+
 
 if __name__ == '__main__':
     unittest.main()
